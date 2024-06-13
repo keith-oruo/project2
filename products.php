c4217401c72da8a5635eb4d95a09b1c722029a7b
keith-oruo <keith.oruo@strathmore.edu>
2024-06-13 11:53:53 +0300

    learning and implementing include_once and required_once for code reuseability

--- products.php
+++ products.php
@@ -6,18 +6,7 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <div class="toplink">
-            <a href="./">Home</a>
-            <a href="about.html">About Us</a>
-            <a href="projects.html">Our Projects</a>
-            <a href="products.html">Our Products</a>
-            <a href="contacts.html">Contact Us</a>
-
-            <div class="toplink-right">
-                <a href="signup.html">Sign Up</a>
-                <a href="signin.html">Sign In</a>
-            </div>
-        </div>
+        <?php include_once("Templates/nav.php"); ?>
         <div class="banner">
             <h1>Welcome</h1> 
         </div>
@@ -94,29 +83,6 @@
                     </tr>
                 </table>
         </div>
-        <div class="side_bar">
-            <!--Heading 2 starts here-->
-            <h2>Short Stories</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <!--Heading 2 ends here-->
-            <ul>
-            <li>HTML</li>
-            <li>Java</li>
-            <li>JavaScript</li>
-            <li>MySQL</li>
-            <li>PHP</li>
-        </ul>
-        <ol type="I" start="4">
-            <li>Back</li>
-            <li>Red</li>
-            <li>Yellow</li>
-            <li>Pink</li>
-            <li>White</li>
-        </ol>
-    </div>
-</div>
-<div class="footer">
-    Copyright BBIT 2.1 &copy; Allrights reserved 
-</div>
-</body>
-</html>
+        <?php include_once("Templates/sidebar.php"); ?>
+        </div>
+ <?php include_once("Templates/footer.php"); ?>
