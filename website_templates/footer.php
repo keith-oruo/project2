b90f16aa13b1fa99a8a972a2a3e88ce89bf00e09
keith-oruo <keith.oruo@strathmore.edu>
2024-06-13 14:13:51 +0300

    identified similar sections to reuse code

---
+++ website_templates/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+        <p>&copy; 2024 TechSolutions. All rights reserved.</p>
+    </div>
---
+++ website_templates/links.php
@@ -0,0 +1,4 @@
+<div class ="toplink">
+    <a href="Home_page.php" >Home</a> | <a href="About_Page.php" >About Us</a> | <a href="Services_Page.php" >Our Services</a> |  <a href="Product_Page.php" >Our Products</a> | <a href="Team_Page.php" >Our Team</a> | <a href="Contact_Page.php" >Contact Us</a> | <a href="Blog_Page.php" >Blog</a>
+    </div>
+    
